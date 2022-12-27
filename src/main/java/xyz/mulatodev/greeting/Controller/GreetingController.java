aefab10ba9eea1fe2b15dda28c98c0d807cfb253
mulatodev <ganaranjo@gmail.com>
2022-12-27 09:11:35 -0500

    Greeting project initial commit

---
+++ src/main/java/xyz/mulatodev/greeting/Controller/GreetingController.java
@@ -0,0 +1,28 @@
+package xyz.mulatodev.greeting.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.mulatodev.greeting.model.Greeting;
+//import xyz.mulatodev.greeting.utils.WeatherExternalRestfulService;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hola, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    //private String weather = "";
+    //private final String response = "";
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "mulato") String name){
+
+        //WeatherExternalRestfulService accu = new WeatherExternalRestfulService();
+        //weather = accu.GetLocationCodeByCityName("Bogotá");
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/java/xyz/mulatodev/greeting/utils/WeatherExternalRestfulService.java
@@ -0,0 +1,40 @@
+package xyz.mulatodev.greeting.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherExternalRestfulService {
+
+    public String GetLocationCodeByCityName(String cityName) {
+
+        String output = "";
+
+        try {
+            URL url = new URL("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=HU0gvteaKdygWPHZt6ZZ6PB53cUZEctm&q="+cityName);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200)
+                throw new RuntimeException("Failed : HTTP Error code: " + conn.getResponseCode());
+
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+
+            while ((output = br.readLine()) != null ){
+                System.out.println(output);
+            }
+            br.close();
+            in.close();
+            conn.disconnect();
+
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+}
